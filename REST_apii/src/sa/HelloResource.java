f155df97162169dc3bda5b27521a9093404f84bb
ovidiovermemenouze <ovidiovermemenouze@192.168.1.15>
2020-12-21 22:54:43 +0100

    Contador

--- REST_apii/src/sa/HelloResource.java
+++ REST_apii/src/sa/HelloResource.java
@@ -1,23 +1,56 @@
 package sa;
 
+/*
 
- 
- 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
- 
+
 @Path("/")
 
 public class HelloResource {
-	private int contador=0;
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String direBonjour() throws ClassNotFoundException {
-    	Conexion.abrirConexion();
-    	contador++;
-        return "Bienvenido a CRUD RESTful 2" + contador;
-        
-    }
+ // static contador;
+private static ArrayList<Integer> contador = new ArrayList<>();
+
+
+   @GET
+   @Produces(MediaType.TEXT_PLAIN)
+   public String direBonjour() throws ClassNotFoundException {
+   	 {
+   		contador.add(1);
+   		
+   	}
+       return "Bienvenido a CRUD RESTful 2 " + contador.size();
+       
+   }
+}
+*/
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/")
+
+public class HelloResource {
+	// static contador;
+	private static int contador = 0;
+
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String direBonjour() throws ClassNotFoundException {
+
+		contador++;
+
+		return "Bienvenido a CRUD RESTful 2 " + "Contador de visitas: " + contador;
+
+	}
 }
